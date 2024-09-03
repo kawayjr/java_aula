c71240c5156508affc9c3bafcdb52d45eb90858a
Kaway JR <168316453+kawayjr@users.noreply.github.com>
2024-09-02 21:12:27 -0300

    aula 9b
    exercicio aula 9b

--- aula 9/src/Main.java
+++ aula 9/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro [3];
+
+        p[0]= new Pessoa ("Masc", "Pedro", 22);
+        p[1]= new Pessoa ("Fem", "Maria", 25);
+
+        l[0] = new Livro ("Apresentado Java", "José Silva", 300, p[0]);
+        l[1] = new Livro ("POO para iniciante", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro ("Java Avançado", "Jeremias Santos", 400, p[0]);
+
+        System.out.println(l[0].detalhes());
     }
 }
--- aula 9/src/Pessoa.java
+++ aula 9/src/Pessoa.java
@@ -1,2 +1,40 @@
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+
+    public void fazerAniver(){
+        this.idade=this.idade + 1;
+    }
+
+    public Pessoa(String sexo, String nome, int idade) {
+        this.sexo = sexo;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
